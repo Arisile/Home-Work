c81ba61fb084bd203f4d4dcc4a395fc9f02d1d59
Arisile <adesilearisilejoye22@gmail.com>
2025-06-09 00:16:16 +0100

    08/06/25

--- src/Component/Portfolio.jsx
+++ src/Component/Portfolio.jsx
@@ -3,15 +3,16 @@ import React from 'react'
 const Portfolio = () => {
   return (
     <div>
-      <section className='h-screen flex px-6  bg-[#0b043a] text-white  py-20 pl-14' 
+      <section className='h-screen  px-6  bg-[#0b043a] text-white  py-20 pl-14 pr-20' 
      id='portfolio'>
+        <h1>Featured Work</h1>
        <div className='flex sm:h-[400px] sm:w-[800px] ring-2 lg:w-screen lg:h-[500px] lg:flex pl-14px'>
           <div className='w-[60%]'>
-            <img className='h-[400px] pt-6 pb-6' src="https://websitedemos.net/web-developer-04/wp-content/uploads/sites/1481/2023/07/work-01.jpg" alt="" />
+            <img className='h-[400px] pt-6 pb-6 lg:w-full lg:h-[500px]' src="https://websitedemos.net/web-developer-04/wp-content/uploads/sites/1481/2023/07/work-01.jpg" alt="" />
           </div>
           <div className='w-[40%] bg-[#181448]'>
-            <p>Full-Stack</p>
-            <h1>Nientex Website Redesign</h1>
+            <p className='text-base pt-6'>Full-Stack</p>
+            <h1 className='sm:text-[30px] lg:text-[40px] font-bold'>Nientex Website Redesign</h1>
             <p>Revamp your online presence with a modern and user-friendly Nientex website redesign that showcases your hotel's luxury amenities and increases bookings.</p>
             <button>Project Detail</button>
           </div>
